278472beec6b1cac6f714bce20d6604776ec38ac
Dan Erbland <danerbland@gmail.com>
2020-02-26 18:35:51 -0500

    added company name completion route for lookahead

--- server/api/companies.js
+++ server/api/companies.js
@@ -1,14 +1,33 @@
 const router = require('express').Router()
+const Op = require('sequelize').Op
+
 const {Company} = require('../db/models')
 
 //return all companies' ticker and name
-router.use('/', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const companies = await Company.findAll({
       attributes: ['ticker', 'name']
     })
     res.json(companies)
 
+  } catch (error) {
+    next(error)
+  }
+})
+
+//for use in search bar lookahead
+router.get('/ticker/:ticker', async (req, res, next) => {
+  try {
+    let companies = await Company.findAll({
+      limit: 10,
+      where: {
+        ticker: {
+          [Op.iLike]: req.params.ticker + '%'
+        }
+      }
+    })
+    res.json(companies)
   } catch (error) {
     next(error)
   }
